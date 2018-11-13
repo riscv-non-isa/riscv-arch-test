797ce6cb945f8567303c4fc78eb5d80f9b817db5
moore <moore@lnx33.impinternal.com>
2018-11-13 11:45:59 +0000

    rename the generated signature file to be of a similar format to the reference file <stub>.reference_output <stub>.signature_output
    add missing reference signature file for rv32ui/srli
    
    change verify.sh to perform a backward link check

--- riscv-test-env/verify.sh
+++ riscv-test-env/verify.sh
@@ -4,27 +4,42 @@ printf "\n\nCompare to reference files ... \n\n";
 FAIL=0
 RUN=0
 
-for f in ${SUITEDIR}/references/*.reference_output;
+for ref in ${SUITEDIR}/references/*.reference_output;
 do 
-    b=$(basename $f)
-    ex=${b//".reference_output"/}
+    base=$(basename ${ref})
+    stub=${base//".reference_output"/}
+    sig=${WORK}/${RISCV_ISA}/${stub}.signature.output
+
     RUN=$((${RUN} + 1))
     
     #
     # Ensure both files exist
     #
-    if [ -f $f ] && [ -f ${WORK}/${ISA}/${b//".reference_output"/"_signature.output"} ]; then 
-        echo -n "Check $(printf %16s ${ex})"
+    if [ -f ${ref} ] && [ -f ${sig} ]; then 
+        echo -n "Check $(printf %16s ${stub})"
     else
-        echo    "Check $(printf %16s ${ex}) ... IGNORE"
+        echo    "Check $(printf %16s ${stub}) ... IGNORE"
         continue
     fi
-    diff --strip-trailing-cr $f ${WORK}/${ISA}/${b//".reference_output"/"_signature.output"} #&> /dev/null
+    diff --strip-trailing-cr ${ref} ${sig} #&> /dev/null
     if [ $? == 0 ]
     then
         echo " ... OK"
     else
         echo " ... FAIL"
+        FAIL=$((${FAIL} + 1))
+    fi
+done
+
+# warn on missing reverse reference
+for sig in ${WORK}/${RISCV_ISA}/*.signature.output; 
+do
+    base=$(basename ${sig})
+    stub=${base//".signature.output"/}
+    ref=${SUITEDIR}/references/${stub}.reference_output
+
+    if [ -f $sig ] && [ ! -f ${ref} ]; then
+        echo "Error: sig ${sig} no corresponding ${ref}"
         FAIL=$((${FAIL} + 1))
     fi
 done
