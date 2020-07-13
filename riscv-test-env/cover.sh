7008fedfb6218b66fe30eb799af3054fa509e725
eroom1966 <moore@imperas.com>
2020-07-13 13:54:32 +0100

    fix additional subsets

--- riscv-test-env/cover.sh
+++ riscv-test-env/cover.sh
@@ -7,6 +7,7 @@
 # RVZicsr     rv32Zicsr
 # RVZifencei  rv32Zifencei
 
+declare -A map
 map[rv32i]=RVI
 map[rv32im]=RVM
 map[rv32imc]=RVIC,RV32IC
@@ -14,8 +15,8 @@ map[rv32Zicsr]=RVZicsr
 map[rv32Zifencei]=RVZifencei
 
 ALL_ISA=$(ls -1 work)
-
 for ISA in ${ALL_ISA}; do
+    echo "Running $ISA"
     RISCV_ISA=${ISA}
     RISCV_CVG=${map[${RISCV_ISA}]}
     YAML="work/${RISCV_ISA}/*.yaml"
