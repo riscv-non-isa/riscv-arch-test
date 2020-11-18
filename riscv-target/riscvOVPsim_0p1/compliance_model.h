f0a6f759f0a6d10d3b501c5671bcbe49d7a5ab4e
Neel Gala <neelgala@incoresemi.com>
2020-11-18 17:33:28 +0530

    fixes for riscvOVPsim in new and old framework

--- riscv-target/riscvOVPsim_0p1/compliance_model.h
+++ riscv-target/riscvOVPsim_0p1/compliance_model.h
@@ -1,3 +1,40 @@
+// RISC-V Compliance Test Header File
+// Copyright (c) 2017, Codasip Ltd. All Rights Reserved.
+// See LICENSE for license details.
+//
+// Description: Common header file for RV32I tests
+
+#ifndef _COMPLIANCE_TEST_H
+#define _COMPLIANCE_TEST_H
+
+#include "riscv_test.h"
+
+//-----------------------------------------------------------------------
+// RV Compliance Macros
+//-----------------------------------------------------------------------
+
+#define RV_COMPLIANCE_HALT                                                    \
+        RVTEST_PASS                                                           \
+
+#define RV_COMPLIANCE_RV32M                                                   \
+        RVTEST_RV32M       
+        
+#define RV_COMPLIANCE_RV64V                                                  \
+        RVTEST_RV64V                                                          \
+
+#define RV_COMPLIANCE_CODE_BEGIN                                              \
+        RVTEST_CODE_BEGIN1                                                     \
+
+#define RV_COMPLIANCE_CODE_END                                                \
+        RVTEST_CODE_END1                                                       \
+
+#define RV_COMPLIANCE_DATA_BEGIN                                              \
+        RVTEST_DATA_BEGIN1                                                     \
+
+#define RV_COMPLIANCE_DATA_END                                                \
+        RVTEST_DATA_END1                                                       \
+
+#endif
 // RISC-V Compliance IO Test Header File
 
 /*
