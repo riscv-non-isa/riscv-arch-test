5ab4852a5585c1f523d52777278cf38fad0e99e3
Yung-Ching Hsiao <ychsiao@andestech.com>
2022-02-09 16:47:38 +0800

    add TEST_RD_OP(): test instructions with a single register operand -- from RISCV-CTG 6aa62192

--- riscv-test-suite/env/arch_test.h
+++ riscv-test-suite/env/arch_test.h
@@ -969,6 +969,13 @@ RVTEST_SIGUPD_F(swreg,destreg,flagreg,offset)
       inst destreg, x2,imm; \
       )
 
+//Tests for instructions with a single register operand
+#define TEST_RD_OP(inst, destreg, reg1, correctval, val1, swreg, offset, testreg) \
+  TEST_CASE(testreg, destreg, correctval, swreg, offset, \
+    LI(reg1, MASK_XLEN(val1)); \
+    inst destreg, reg1; \
+  )
+
 #define TEST_CBRANCH_OP(inst, tempreg, reg2, val2, imm, label, swreg, offset) \
     LI(reg2, MASK_XLEN(val2))                  ;\
     j 2f                                      ;\
