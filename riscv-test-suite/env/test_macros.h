29362ab60781093f04b51ccfd72272b82dbca65c
ammar <ammar.sarwar@10xengineers.ai>
2023-09-29 22:25:16 +0500

    Adding macros to setup PTE for RV64

--- riscv-test-suite/env/test_macros.h
+++ riscv-test-suite/env/test_macros.h
@@ -41,6 +41,68 @@
     add _TR1, _TR1, _TR0                                        ;\
     SREG _PAR, 0(_TR1);                                          
 
+#define PTE_SETUP_RV64(_PAR, _PR, _TR0, _TR1, VA, level, mode)  ;\
+    srli _PAR, _PAR, 12                                         ;\
+    slli _PAR, _PAR, 10                                         ;\
+    or _PAR, _PAR, _PR                                          ;\
+    .if (mode == sv39)                                          ;\
+        .if (level == 2)                                        ;\
+            LA(_TR1, rvtest_Sroot_pg_tbl)                       ;\
+            .set vpn, ((VA >> 30) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 1)                                        ;\
+            LA(_TR1, rvtest_slvl1_pg_tbl)                       ;\
+            .set vpn, ((VA >> 21) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 0)                                        ;\
+            LA(_TR1, rvtest_slvl2_pg_tbl)                       ;\
+            .set vpn, ((VA >> 12) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+    .endif                                                      ;\
+    .if (mode == sv48)                                          ;\
+        .if (level == 3)                                        ;\
+            LA(_TR1, rvtest_Sroot_pg_tbl)                       ;\
+            .set vpn, ((VA >> 39) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 2)                                        ;\
+            LA(_TR1, rvtest_slvl1_pg_tbl)                       ;\
+            .set vpn, ((VA >> 30) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 1)                                        ;\
+            LA(_TR1, rvtest_slvl2_pg_tbl)                       ;\
+            .set vpn, ((VA >> 21) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 0)                                        ;\
+            LA(_TR1, rvtest_slvl3_pg_tbl)                       ;\
+            .set vpn, ((VA >> 12) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+    .endif                                                      ;\
+    .if (mode == sv57)                                          ;\
+        .if (level == 4)                                        ;\
+            LA(_TR1, rvtest_Sroot_pg_tbl)                       ;\
+            .set vpn, ((VA >> 48) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 3)                                        ;\
+            LA(_TR1, rvtest_slvl1_pg_tbl)                       ;\
+            .set vpn, ((VA >> 39) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 2)                                        ;\
+            LA(_TR1, rvtest_slvl2_pg_tbl)                       ;\
+            .set vpn, ((VA >> 30) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 1)                                        ;\
+            LA(_TR1, rvtest_slvl3_pg_tbl)                       ;\
+            .set vpn, ((VA >> 21) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+        .if (level == 0)                                        ;\
+            LA(_TR1, rvtest_slvl3_pg_tbl)                       ;\
+            .set vpn, ((VA >> 12) & 0x1FF) << 3                 ;\
+        .endif                                                  ;\
+    .endif                                                      ;\
+    LI(_TR0, vpn)                                               ;\
+    add _TR1, _TR1, _TR0                                        ;\
+    SREG _PAR, 0(_TR1)                                          ;
+
 #define PTE_PERMUPD_RV32(_PR, _TR0, _TR1, VA, level)          	;\
     .if (level==1)                                              ;\
         LA(_TR1, rvtest_Sroot_pg_tbl)                           ;\
@@ -64,6 +126,24 @@
     srli t6, t6, 12 ;\
     or t6, t6, t5  ;\
     csrw satp, t6   ;\
+
+#define SATP_SETUP_RV64(MODE)                                   ;\
+    LA(t6, rvtest_Sroot_pg_tbl)                                 ;\
+    .if (MODE == sv39)                                          ;\
+    LI(t5, (SATP64_MODE) & (SATP_MODE_SV39 << 60))              ;\
+    .endif                                                      ;\
+    .if (MODE == sv48)                                          ;\
+    LI(t5, (SATP64_MODE) & (SATP_MODE_SV48 << 60))              ;\
+    .endif                                                      ;\
+    .if (MODE == sv57)                                          ;\
+    LI(t5, (SATP64_MODE) & (SATP_MODE_SV57 << 60))              ;\
+    .endif                                                      ;\
+    .if (MODE == sv64)                                          ;\
+    LI(t5, (SATP64_MODE) & (SATP_MODE_SV64 << 60))              ;\
+    .endif                                                      ;\
+    srli t6, t6, 12                                             ;\
+    or t6, t6, t5                                               ;\
+    csrw satp, t6                                               ;
 
 #define NAN_BOXED(__val__,__width__,__max__)	;\
     .if __width__ == 32				;\
