17158ba58c33ab519bfacec7d856ee5c02b043ab
Jordan Carlin <jcarlin@qti.qualcomm.com>
2026-09-11 16:22:30 -0700

    Select test CSR for Zicsr tests with priv mode fallback
    Fixes #1572
    Replaces #1935
    
    Co-authored-by: karabambus <mradic07@gmail.com>
    Signed-off-by: Jordan Carlin <jcarlin@qti.qualcomm.com>

--- generators/testgen/src/testgen/formatters/types/csri_type.py
+++ generators/testgen/src/testgen/formatters/types/csri_type.py
@@ -16,31 +16,21 @@ csri_config = InstructionTypeConfig(required_params={"rd", "rs2", "rs2val", "imm
 
 def zicsr_accessi(instr_name: str, rd: int, rs2: int, immval: int) -> str:
     """Helper function to determine which CSR to use for testing based on supported extensions."""
-    # Use writable unprivileged extension CSRs if any exist,
-    # else use mepc if U is not supported
-    # else use instret (which is not writable, but at least can be accessed)
-
     # instret requires special treatment because it is not writable, and the value is not initialized
     if instr_name in ["csrrw", "csrrwi"] or rs2 == 0 or rd == 0:
-        instret_access = f"li x{rd}, 0 # avoid write to read-only instret, or inconsistent result with rs2 or rd = 0"
+        read_only_access = f"li x{rd}, 0 # avoid write to read-only CSR, or inconsistent result with rs2 or rd = 0"
     else:
-        instret_access = (
-            f"{instr_name} x{rs2}, instret, 0\n"
-            f"{instr_name} x{rd}, instret, 0\n"
-            f"sub x{rd}, x{rd}, x{rs2}  # check that instret value has incremented\n"
+        read_only_access = (
+            f"{instr_name} x{rs2}, RVTEST_TEST_CSR, 0\n"
+            f"{instr_name} x{rd}, RVTEST_TEST_CSR, 0\n"
+            f"sub x{rd}, x{rd}, x{rs2}  # check that the read-only CSR value has incremented\n"
         )
 
     return (
-        "#if defined(F_SUPPORTED)\n"
-        f"{instr_name} x{rd}, fflags, {immval}\n"
-        "#elif defined(V_SUPPORTED)\n"
-        f"{instr_name} x{rd}, vxsat, {immval}\n"
-        "#elif !defined(U_SUPPORTED)\n"
-        f"{instr_name} x{rd}, mepc, {immval}\n"
-        "#elif defined(ZICNTR_SUPPORTED)\n"
-        f"{instret_access}\n"
+        "#ifdef RVTEST_READ_ONLY_TEST_CSR\n"
+        f"{read_only_access}\n"
         "#else\n"
-        f"  #error no CSR known for testing\n"
+        f"{instr_name} x{rd}, RVTEST_TEST_CSR, {immval}\n"
         "#endif\n"
     )
 
--- tests/env/utils.h
+++ tests/env/utils.h
@@ -225,6 +225,23 @@
 #define NOP                    0x13
 #define DOUBLE_NOP             (0x13<<32)+0x13
 
+// Determine the appropriate CSR to test based on the
+// supported extensions and set boot mode if necessary.
+#if defined(F_SUPPORTED)
+  #define RVTEST_TEST_CSR fflags
+#elif defined(ZVE32X_SUPPORTED)
+  #define RVTEST_TEST_CSR vxsat
+#elif defined(ZICNTR_SUPPORTED) && defined(U_SUPPORTED)
+  #define RVTEST_TEST_CSR instret
+  #define RVTEST_READ_ONLY_TEST_CSR
+#elif defined(S_SUPPORTED)
+  #define RVTEST_TEST_CSR sepc
+  #define BOOT_TO_SMODE
+#else
+  #define RVTEST_TEST_CSR mepc
+  #define BOOT_TO_MMODE
+#endif
+
 // RVTEST_TESTDATA_LOAD_INT(data_ptr, dest_reg) loads an integer value from the
 // test data section into dest_reg and increments the data_ptr pointer by SIG_STRIDE.
 // This macro is used to load integer test values from the .data section.
